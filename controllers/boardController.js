207b02afe9586df79a1f4073978c868b7c7f5986
de Priego Laureano <laureano.1998@gmail.com>
2024-12-11 18:39:48 -0300

    cambios en operacion delete del boardController

--- controllers/boardController.js
+++ controllers/boardController.js
@@ -40,17 +40,22 @@ exports.updateBoard = async (req, res) => {
   }
 };
 
+
 exports.deleteBoard = async (req, res) => {
   try {
     const board = await Board.findById(req.params.id);
+
     if (!board || board.owner.toString() !== req.user.id) {
       return res
         .status(404)
         .json({ error: "Tablero no encontrado o no autorizado" });
     }
-    await board.remove();
+
+    await Board.findByIdAndDelete(req.params.id);
+
     res.status(200).json({ message: "Tablero eliminado" });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: "Error al eliminar el tablero" });
   }
 };
